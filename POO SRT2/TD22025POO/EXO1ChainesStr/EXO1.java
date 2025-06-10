45d37ad0a5e7d7dd5c4a41cbdfcf37c7c5f163bd
drisstein <idrissahmad1903@gmail.com>
2025-06-10 23:12:50 +0000

    Ajout de l'exercice 1

---
+++ POO SRT2/TD22025POO/EXO1ChainesStr/EXO1.java
@@ -0,0 +1,25 @@
+/*
+ * Exercice 1 : Manipuler les chaînes de caractères et extraire des informations. Écrire un programme qui prend une URL (ex: "https://uadb.sn") et affiche : 
+- Le protocole (https) 
+-Le nom de domaine (uadb) 
+-L’extension (sn) 
+👉 Utilisez les méthodes de la classe String : split(), substring(), indexOf()
+ */
+
+
+public class EXO1 {
+    public static void main(String[] args) {
+        String url = "https://uadb.sn";
+        String protocol = url.substring(0, url.indexOf(":"));
+        String protocol2 = url.substring(0, 4);
+
+        String domain = url.substring(url.indexOf("//") + 2, url.lastIndexOf("."));
+        String extension = url.substring(url.lastIndexOf(".") + 1);
+
+        System.out.println("Protocole M1 avec indexOf: " + protocol);
+        System.out.println("Protocole M2 substring(0,4): " + protocol2);
+        System.out.println("Nom de domaine : " + domain);
+        System.out.println("Extension : " + extension);
+
+    }
+}
